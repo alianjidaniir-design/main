0cc3e560b55a8434f866236a2ddde32b3af78166
Mahdi Anjidani <>
2025-11-04 10:18:28 +0330

    branch

--- usep/sqlite.go
+++ usep/sqlite.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"strings"
+	"time"
 
 	"github.com/alianjidaniir-design/sqlite06"
 )
@@ -32,5 +35,51 @@ func getstring(length int64) string {
 func main() {
 
 	sqlite06.Filename = "ch06.db"
+	data, err := sqlite06.ListUsers()
+	if err != nil {
+		fmt.Println("ListUsers():", err)
+		return
+	}
+	if len(data) != 0 {
+		for _, v := range data {
+			fmt.Println(v)
+		}
+	}
+	random_username := strings.ToLower(getstring(5))
+	t := sqlite06.Userdata{
+		Username:    random_username,
+		Name:        "Ali",
+		Surname:     "Anjidani",
+		Description: "This is for me",
+	}
+	fmt.Println("Adding  username", random_username)
+	id := sqlite06.AddUser(t)
+	if id == -1 {
+		fmt.Println("There was an error adding user", t.Username)
+	}
+	err = sqlite06.DeleteUser(id)
+	if err != nil {
+		fmt.Println("DeleteUser:", err)
+	} else {
+		fmt.Println("User with ID:", id, "Deleted!")
+	}
+	err = sqlite06.DeleteUser(id)
+	if err != nil {
+		fmt.Println("DeleteUser:", err)
+	}
+	random_username = strings.ToLower(getstring(5))
+	random_name := getstring(7)
+	random_surname := getstring(10)
+	dsc := time.Now().Format("2006-01-02 15:04:05")
+	t := sqlite06.Userdata{
+		Username:    random_name,
+		Name:        random_name,
+		Surname:     random_surname,
+		Description: dsc,
+	}
+	id := sqlite06.AddUser(t)
+	if id == -1 {
+		fmt.Println("There was an error adding user", t.Username)
+	}
 
 }
