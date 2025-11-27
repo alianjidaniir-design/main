0f3661e8fd755e4c0b54f93dd63ebe7d42404cee
Mahdi Anjidani <>
2025-11-27 13:47:16 +0330

    brach

--- g.go
+++ g.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
+
+func pm(xx int) int {
+	return xx * xx
+}
 
 func main() {
 	fmt.Println("runtime.Compiler,", runtime.Compiler, " ")
 	fmt.Println("on a", runtime.GOOS, "machine")
 	fmt.Println(runtime.Version())
-	fmt.Printf("GOMAXPROCS=%d\n", runtime.GOMAXPROCS(0))
+	fmt.Printf("GOMAXPROCS=%d\n", runtime.GOMAXPROCS(1))
+
+	go func(x int) {
+		fmt.Println(123)
+	}(5)
+	go pm(13)
+	go pm(14)
+	time.Sleep(time.Second)
+	fmt.Println("Exiting")
+
 }
