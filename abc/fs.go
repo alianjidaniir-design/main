9b26d6357fa646d35ee6d664bf8db300b7c57909
Mahdi Anjidani <>
2025-11-27 13:34:18 +0330

    branch

---
+++ abc/fs.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+)
+
+func list(f embed.FS) {
+	return fs.WalkDir()
+}
+
+func main() {
+
+}
--- g.go
+++ g.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("runtime.Compiler,", runtime.Compiler, " ")
+	fmt.Println("on a", runtime.GOOS, "machine")
+	fmt.Println(runtime.Version())
+	fmt.Printf("GOMAXPROCS=%d\n", runtime.GOMAXPROCS(0))
+}
